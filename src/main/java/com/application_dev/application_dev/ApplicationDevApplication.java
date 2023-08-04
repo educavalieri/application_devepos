6575cc2b072fe3f41a44c968a28e5fdb8055bd99
Eduardo Adriano Cavalieri <e.adriano.cavalieri@avanade.com>
2023-08-04 14:25:17 -0300

    init project

---
+++ src/main/java/com/application_dev/application_dev/ApplicationDevApplication.java
@@ -0,0 +1,14 @@
+package com.application_dev.application_dev;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application_dev/application_dev/ApplicationDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application_dev.application_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
