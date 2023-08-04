cce8e9d2b72c6961f6503bf48e69630c1d2d0529
educavalieri <D@rkman1969>
2023-08-04 16:59:15 -0300

    Insert wellcome class

---
+++ src/main/java/com/application_dev/application_dev/controller/WellComeController.java
@@ -0,0 +1,18 @@
+package com.application_dev.application_dev.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/wellcome")
+public class WellComeController {
+
+    @GetMapping
+    public String welcome(){
+        return "well come young Padawan";
+    }
+
+}
