58d27049c317e88cada96d81cbf0988b6f5518f4
educavalieri <D@rkman1969>
2024-03-28 14:39:42 -0300

    teste de reload 3

--- src/main/java/com/application_dev/application_dev/controller/WellComeController.java
+++ src/main/java/com/application_dev/application_dev/controller/WellComeController.java
@@ -30,7 +30,7 @@ public class WellComeController {
     @GetMapping
     @RequestMapping(value = "/teste")
     public String teste(){
-        return "teste ok ok ok";
+        return "teste";
     }
 
     @GetMapping
